62783999c88a211a28b8c8bd11bf72a292ec00b3
Jean Sacren <sakiwit@gmail.com>
2025-12-01 22:43:08 -0700

    Build stocks route

--- app/Http/Controllers/StockController.php
+++ app/Http/Controllers/StockController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class StockController extends Controller
 {
@@ -11,7 +12,7 @@ class StockController extends Controller
      */
     public function index()
     {
-        //
+        return Inertia::render('stocks/Index');
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\StockController;
 use App\Http\Controllers\PizzaController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
@@ -13,4 +14,7 @@ Route::resource('pizzas', PizzaController::class)
     ->only(['index']);
 
 Route::resource('users', UserController::class)
+    ->only(['index']);
+
+Route::resource('stocks', StockController::class)
     ->only(['index']);
