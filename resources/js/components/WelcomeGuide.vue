28de2b858f49b381b5ab776b188071b28f3a8ec9
Jean Sacren <sakiwit@gmail.com>
2025-12-02 22:41:15 -0700

    Build WelcomeGuide vue component

---
+++ resources/js/components/WelcomeGuide.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+    import WelcomeItem from '@/components/WelcomeItem.vue'
+    import DocumentationIcon from '@/components/icons/IconDocumentation.vue'
+</script>
+
+<script lang="ts">
+    export default {
+        name: 'WelcomeGuide'
+    }
+</script>
+
+<template>
+    <WelcomeItem>
+        <template #icon><DocumentationIcon /></template>
+        <template #heading>Documentation</template>
+
+        Vue’s
+        <a href="https://vuejs.org/" target="_blank" rel="noopener" class="text-blue-500 hover:underline">
+            official documentation
+        </a>
+        provides you with all information you need to get started.
+    </WelcomeItem>
+</template>
+
