13d004d4e5330711c6f6522d1344d69d73090a32
Jean Sacren <sakiwit@gmail.com>
2025-12-30 13:09:09 -0700

    Build Footer vue component

---
+++ resources/js/components/Footer.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+const year = '2025';
+</script>
+
+<template>
+    <footer class="p-4 text-center text-sm text-blue-500">
+        <p>&copy; {{ year }}</p>
+    </footer>
+</template>
