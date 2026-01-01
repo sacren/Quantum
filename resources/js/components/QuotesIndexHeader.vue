7cad54538e523cedb22525f31880253455e2d664
Jean Sacren <sakiwit@gmail.com>
2026-01-01 09:54:38 -0700

    Use ref() for reactivity
    Import, call ref() and display the reactive data.  But it is not a good
    one for reactivity as the data is static.

--- resources/js/components/QuotesIndexHeader.vue
+++ resources/js/components/QuotesIndexHeader.vue
@@ -1,8 +1,11 @@
 <script setup lang="ts">
+import { ref } from 'vue';
+
+const title = ref('Quote of the day');
 </script>
 
 <template>
     <header class="text-slate-200 mb-6">
-        <h1>Quote Generator</h1>
+        <h1>{{ title }}</h1>
     </header>
 </template>
