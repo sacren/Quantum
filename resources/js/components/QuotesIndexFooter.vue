3d7eac1b18a3536850329ebab3a9fd56876e0407
Jean Sacren <sakiwit@gmail.com>
2025-12-31 23:46:17 -0700

    Build QuotesIndexFooter vue component

---
+++ resources/js/components/QuotesIndexFooter.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <footer class="fixed bottom-0 w-full text-center text-slate-200 pb-5">
+        Created by Professor Doolittle &copy; 2025
+    </footer>
+</template>
