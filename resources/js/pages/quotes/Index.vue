8b951cce862f5b82fe089fac02c8f8dbbaab63c3
Jean Sacren <sakiwit@gmail.com>
2026-01-01 08:43:53 -0700

    Build Index vue component for quotes

---
+++ resources/js/pages/quotes/Index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import QuotesIndexHeader from '@/components/QuotesIndexHeader.vue'
+import QuotesIndexFooter from '@/components/QuotesIndexFooter.vue'
+import QuotesList from '@/components/QuotesList.vue'
+</script>
+
+<template>
+    <div class="min-h-screen bg-slate-600 flex flex-col items-center p-6">
+        <QuotesIndexHeader />
+        <QuotesList />
+        <QuotesIndexFooter />
+    </div>
+</template>
