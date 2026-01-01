933f2152f9f18498d3130e3bc65a5902dc136faf
Jean Sacren <sakiwit@gmail.com>
2026-01-01 12:50:18 -0700

    Use .value to override
    When we use ref(), we encapsulate the data and we can use .value to
    override the value of that data.

--- resources/js/components/QuotesList.vue
+++ resources/js/components/QuotesList.vue
@@ -1,4 +1,11 @@
 <script setup lang="ts">
+import { ref } from 'vue';
+
+const quote = ref('First solve the problem. Then write the code.');
+quote.value = 'Given enough eyeballs, all bugs are shallow.';
+
+const author = ref('John Johnson');
+author.value = 'Eric S. Raymond';
 </script>
 
 <template>
@@ -6,10 +13,10 @@
         <section class="text-center max-w-md">
             <div class="bg-white rounded-xl p-6 mb-4 shadow-lg">
                 <p class="text-slate-800 text-lg md:text-xl italic">
-                    First solve the problem. Then write the code.
+                    {{ quote }}
                 </p>
             </div>
-            <span class="text-slate-300 font-medium">— John Johnson</span>
+            <span class="text-slate-300 font-medium">— {{ author }}</span>
         </section>
         <button
             class="mt-8 px-6 py-2 bg-slate-700 hover:bg-slate-600 text-slate-200 font-medium rounded-lg transition-colors duration-200"
